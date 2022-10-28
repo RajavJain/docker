ad654d82d9ffc759c02a2836b6f19567f9ec2180
RajavJain <rajavjain2002@gmail.com>
2022-10-29 01:03:46 +0530

    express server

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,12 @@
 const connectToMongo = require("./db") //imported server through db.js
+const express = require('express')
 
 connectToMongo();
+
+const app = express();
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
