85df43653b0566264eab298bc898c96c36fc1e89
RajavJain <rajavjain2002@gmail.com>
2022-11-03 19:29:42 +0530

    Done with route for adding notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,18 +1,58 @@
 const express = require("express");
-const router =express.Router();//
-const fetchuser= require('../middleware/fetchuser')
-const Notes = require('../models/Notes')//imported notes schema
+const router = express.Router();//
+const fetchuser = require('../middleware/fetchuser')
+const Note = require('../models/Note')//imported notes schema
+const { body, validationResult } = require('express-validator');//used for validating the input, syntax from express-validator site
 
 
 
 //ROUTE-----------1-----------
 
+//Get all the notes using GET : ENDPOINT:"/api/auth/fetchallnotes"    LOGIN REQUIRED        <<<<<<<<<-------------------------->>>>>>>>>>>>
+router.get('/fetchallnotes', fetchuser, async (req, res) => {
 
-//Get all the notes using GET : ENDPOINT:"/api/auth/fetchallnotes"    NO LOGIN REQUIRED        <<<<<<<<<-------------------------->>>>>>>>>>>>
-router.get('/fetchallnotes',fetchuser, async (req,res) =>{
-    const notes= await Notes.find({user: req.user.id})//ye islye kr paaye kyuki isme hamne fetchuser ko import kr liya h
-    
-    res.json(notes);
+    try {
+        //it will find the user using ID
+        const notes = await Note.find({ user: req.user.id })//ye islye kr paaye kyuki isme hamne fetchuser ko import kr liya h
+        res.json(notes);
+    }
+    catch (error) {
+        console.error(error.message)
+        res.status(500).send("Some Error occured from our side!");
+    }
 })
 
-module.exports =router;
+
+
+//ROUTE-----------2-----------
+
+//Adding new notes using POST : ENDPOINT:"/api/auth/addnote"     LOGIN REQUIRED        <<<<<<<<<-------------------------->>>>>>>>>>>>
+router.post('/addnote', fetchuser, [
+    //isme ek array mai saari conditions hi paas kr di hai... with the use of express-validator from it's site
+    body('title', 'Enter a valid title').isLength({ min: 3 }),
+    body('description', 'Description must be atleast 5 character').isLength({ min: 5 })
+],
+    async (req, res) => {
+        try {
+            const { title, description, tag } = req.body;
+            const errors = validationResult(req);//took the code from express-validator, more optimized one, if errors then it will return error
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+
+            const note = new Note({
+                title, description, tag, user: req.user.id
+            })
+            const savedNote = await note.save();
+
+
+            res.json(savedNote);
+        } catch (error) {
+            console.error(error.message)
+            res.status(500).send("Some Error occured from our side!");
+        }
+    })
+
+
+
+module.exports = router;
