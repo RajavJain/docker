ba59c4fbb4aa0ee92cc85c99e6d752cb60ad0225
RajavJain <rajavjain2002@gmail.com>
2022-11-03 18:33:06 +0530

    Done with route for fetching notes

--- backend/models/Notes.js
+++ backend/models/Notes.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose');
+const {Schema} = mongoose;//imported Schema from mongoose for exporting purpose as it was giving error
+
 
 const NotesSchema = new Schema({
+    //ye bs isliye daala hai taaki kisi tarah user(jaise yaha par userId link kr di hai hamne) and notes aapas mai relate ho paae... to have security, kahin aisa na ho ki kisi aur k note kisi doosre ko dikh jaae...
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+
+
     title:{
         type: String,
         required: true
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const router =express.Router();//
 const fetchuser= require('../middleware/fetchuser')
+const Notes = require('../models/Notes')//imported notes schema
 
 
 
 //ROUTE-----------1-----------
 
 
-//Create a User using POST: ENDPOINT:"/api/auth/createuser"    NO LOGIN REQUIRED        <<<<<<<<<-------------------------->>>>>>>>>>>>
-router.get('/fetchallnotes',fetchuser, (req,res) =>{
-    res.json([])
+//Get all the notes using GET : ENDPOINT:"/api/auth/fetchallnotes"    NO LOGIN REQUIRED        <<<<<<<<<-------------------------->>>>>>>>>>>>
+router.get('/fetchallnotes',fetchuser, async (req,res) =>{
+    const notes= await Notes.find({user: req.user.id})//ye islye kr paaye kyuki isme hamne fetchuser ko import kr liya h
+    
+    res.json(notes);
 })
 
 module.exports =router;
