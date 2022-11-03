62c113b46d828f648c02a7454a1affae43bb2396
RajavJain <rajavjain2002@gmail.com>
2022-11-03 21:20:41 +0530

    Done with Backend and it's API's

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -11,7 +11,7 @@ const { body, validationResult } = require('express-validator');//used for valid
 
 //ROUTE-----------I-----------
 
-//Get all the notes using GET : ENDPOINT:"/api/auth/fetchallnotes"    LOGIN REQUIRED        <<<<<<<<<-------------------------->>>>>>>>>>>>
+//Get all the notes using GET : ENDPOINT:"/api/note/fetchallnotes"    LOGIN REQUIRED        <<<<<<<<<-------------------------->>>>>>>>>>>>
 router.get('/fetchallnotes', fetchuser, async (req, res) => {
 
     try {
@@ -31,7 +31,7 @@ router.get('/fetchallnotes', fetchuser, async (req, res) => {
 
 //ROUTE-----------II-----------
 
-//Adding new notes using POST : ENDPOINT:"/api/auth/addnote"     LOGIN REQUIRED        <<<<<<<<<-------------------------->>>>>>>>>>>>
+//Adding new notes using POST : ENDPOINT:"/api/note/addnote"     LOGIN REQUIRED        <<<<<<<<<-------------------------->>>>>>>>>>>>
 router.post('/addnote', fetchuser, [
     //isme ek array mai saari conditions hi paas kr di hai... with the use of express-validator from it's site
     body('title', 'Enter a valid title').isLength({ min: 3 }),
@@ -63,38 +63,68 @@ router.post('/addnote', fetchuser, [
 
 //ROUTE-----------III-----------
 
-//Updating new notes using PUT : ENDPOINT:"/api/auth/updatenote"     LOGIN REQUIRED        <<<<<<<<<-------------------------->>>>>>>>>>>>
+//Updating new notes using PUT : ENDPOINT:"/api/note/updatenote"     LOGIN REQUIRED        <<<<<<<<<-------------------------->>>>>>>>>>>>
 router.put('/updatenote/:id', fetchuser, async (req, res) => {
-        const {title,description, tag} = req.body;//destructured all the inputs given by the user
-        //Creating a newNote object----
+    const { title, description, tag } = req.body;//destructured all the inputs given by the user
+    //Creating a newNote object----
+    try {
         let newNote = {};
-        if(title)
-        {
-            newNote.title=title;
+        if (title) {
+            newNote.title = title;
         }
-        if(description)
-        {
-            newNote.description=description;
+        if (description) {
+            newNote.description = description;
         }
-        if(tag)
-        {
-            newNote.tag=tag;
+        if (tag) {
+            newNote.tag = tag;
         }
 
         //Finding the note to be updated and updating it....
         let note = await Note.findById(req.params.id);
         //ye sab to pehle ye dekhne k liye kr rhe hai ki user apne hi notes access kr rha hai ki nhi...
-        if(!note)
-        {
-           return res.status(404).send("Not Found");
+        if (!note) {
+            return res.status(404).send("Not Found");
         }
-        if(note.user.toString() !== req.user.id) {
+        if (note.user.toString() !== req.user.id) {
             return res.status(401).send("Not Allowed");
         }
 
-        note= await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true})
-        res.json({note});
+        note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+        res.json({ note });
 
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("Some Error occured from our side!");
+    }
+
+})
+
+
+//-----------------------------------------------------/////--------------------------------------------------
+
+//ROUTE-----------IV-----------
+
+//Deleting new notes using DELETE : ENDPOINT:"/api/note/updatenote"     LOGIN REQUIRED        <<<<<<<<<-------------------------->>>>>>>>>>>>
+//Similar to update route
+router.delete('/deletenote/:id', fetchuser, async (req, res) => {
+    try {
+        //Finding the note to be delete and deleting it....
+        let note = await Note.findById(req.params.id);
+        //ye sab to pehle ye dekhne k liye kr rhe hai ki user apne hi notes access kr rha hai ki nhi...
+        if (!note) {
+            return res.status(404).send("Not Found");
+        }
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allowed");
+        }
+
+        note = await Note.findByIdAndDelete(req.params.id)
+        res.json({ "Success": "Note has been deleted", note: note })
+
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("Some Error occured from our side!");
+    }
 })
 
 
