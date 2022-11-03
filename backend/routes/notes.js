d4068c93fd7b0bc699d0817a43284779131b7800
RajavJain <rajavjain2002@gmail.com>
2022-11-03 18:19:06 +0530

    Added route for fetching notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router =express.Router();//
+const fetchuser= require('../middleware/fetchuser')
 
-router.get('/',(req,res) =>{
+
+
+//ROUTE-----------1-----------
+
+
+//Create a User using POST: ENDPOINT:"/api/auth/createuser"    NO LOGIN REQUIRED        <<<<<<<<<-------------------------->>>>>>>>>>>>
+router.get('/fetchallnotes',fetchuser, (req,res) =>{
     res.json([])
 })
 
