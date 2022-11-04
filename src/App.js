60303b1d7b4a1a704547d49bfa504ee32652c001
RajavJain <rajavjain2002@gmail.com>
2022-11-04 17:15:23 +0530

    Added About, Home, Navbar components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import React from "react";
+import Home from './components/Home';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Navbar from './components/Navbar';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
