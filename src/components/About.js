f358bf1e48e173c4222ae18fba2aedc8615bf792
RajavJain <rajavjain2002@gmail.com>
2022-11-05 21:41:32 +0530

    Added useContext Concept

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,19 @@
-import React, {useContext} from 'react'
+import React, {useContext, useEffect} from 'react'
 import noteContext from '../context/notes/noteContext'
 
 
 
 const About = () => {
-  const a= useContext(noteContext)
+  const a= useContext(noteContext);
+  useEffect(() => {
+    a.update();
+    // eslint-disable-next-line 
+  }, [])
+  
   return (
     <div>
         {/* ye hum iss state ko abb use kr skte hai through the concept of Context */}
-        This is About {a.name}
+        This is about {a.state.name} and currently doing BTech in {a.state.branch}
     </div>
   )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,14 +1,27 @@
 import React from "react";
 import NoteContext from "./noteContext";// ye import ho gya hai noteContext
+import { useState } from "react";
 
-const NoteState =(props)=>{
+const NoteState = (props) => {
     //jo ye humne state banaya hai wo abb koi bhi access kr skta hai... and this is the concept of Context...
-    const state={
+    const s1 = {
         "name": "Rajav",
         "branch": "EE"
     }
+
+    const [state, setState] = useState(s1);
+    const update = () => {
+        setTimeout(() => {
+            setState({
+                "name": "Rajav jain",
+                "branch": "Still EE"
+            })
+        }, 1000);
+    }
+
     return (
-        <NoteContext.Provider value={state}>
+        // isme hamne state, update send kr diya hai mtlb ki hum context ki madad se kisi function ko bhi call kr skte hain, here update is a function....
+        <NoteContext.Provider value={{state,update}}>
             {props.children}
         </NoteContext.Provider>
     )
