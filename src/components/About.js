18516c2601643f418e06e7d79fd7730197ed8b48
RajavJain <rajavjain2002@gmail.com>
2022-11-12 22:25:47 +0530

    Added About section

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,94 @@
 import React from 'react'
+import "../App.css"
 
 
 const About = () => {
   return (
     <div>
-        This is about page
-    </div>
-  )
+      <hr className="featurette-divider"/>
+
+        <div className="container1 mr-10 d-flex justify-content-center align-items-center">
+          <div className="row featurette ">
+            <div className="col-md-7 ">
+              <h2 className="featurette-heading">
+                Lorem, ipsum dolor.
+                <span className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate, repellat!</span>
+              </h2>
+              <p className="lead">
+                Some great placeholder content for the first featurette here.
+                Imagine some exciting prose here. Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, molestiae expedita cum animi voluptatum repellat earum voluptas reprehenderit esse laborum est, exercitationem numquam quibusdam fuga in odio quam libero quaerat.
+              </p>
+            </div>
+            <div className="col-md-5">
+              <img src="https://images.unsplash.com/photo-1621409791205-f3ce9280b5e9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fHRyYWRpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60" alt="" height="300px"
+                width="500px"/>
+                <title>Placeholder</title>
+                <rect width="100%" height="100%" fill="#eee"></rect>
+                <text x="50%" y="50%" fill="#aaa" dy=".3em"></text>
+              
+            </div>
+          </div>
+        </div>
+
+
+        <hr className="featurette-divider"/>
+
+
+          <div className="container1 mr-10 d-flex justify-content-center align-items-center">
+            <div className="row featurette">
+              <div className="col-md-7 order-md-2">
+                <h2 className="featurette-heading">
+                  Lorem, ipsum dolor.
+                  <span className="text-muted">It ll blow your mind.</span>
+                </h2>
+                <p className="lead">
+                  Some great placeholder content for the first featurette here.
+                  Imagine some exciting prose here.
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci dolores magni ut. Saepe perferendis et placeat sapiente ut voluptatibus, quis magni nesciunt delectus, deserunt totam dolorum unde iste, aliquid corporis.
+                </p>
+              </div>
+              <div className="col-md-5">
+                <img src="https://images.unsplash.com/photo-1638913662380-9799def8ffb1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxzZWFyY2h8MXx8dHJhZGluZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60" alt="" height="300px"
+                  width="500px"/>
+                  <title>Placeholder</title>
+                  <rect width="100%" height="100%" fill="#eee"></rect>
+                  <text x="50%" y="50%" fill="#aaa" dy=".3em"></text>
+              </div>
+            </div>
+          </div>
+
+
+
+
+          <hr className="featurette-divider"/>
+
+
+            <div className="container1 mr-10 d-flex justify-content-center align-items-center">
+              <div className="row featurette">
+                <div className="col-md-7">
+                  <h2 className="featurette-heading">
+                    Lorem, ipsum dolor.
+                    <span className="text-muted">It ll blow your mind.</span>
+                  </h2>
+                  <p className="lead">
+                    Some great placeholder content for the first featurette here.
+                    Imagine some exciting prose here.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci dolores magni ut. Saepe perferendis et placeat sapiente ut voluptatibus, quis magni nesciunt delectus, deserunt totam dolorum unde iste, aliquid corporis.
+                  </p>
+                </div>
+                <div className="col-md-5">
+                  <img src="https://images.unsplash.com/photo-1563986768711-b3bde3dc821e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8dHJhZGluZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60" alt="" height="300px"
+                    width="500px"/>
+                    <title>Placeholder</title>
+                    <rect width="100%" height="100%" fill="#eee"></rect>
+                    <text x="50%" y="50%" fill="#aaa" dy=".3em"></text>
+                </div>
+              </div>
+            </div>
+
+            <hr className="featurette-divider"/>
+            </div>
+            )
 }
 
-export default About
+            export default About
