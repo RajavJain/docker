ec86321548d8327f7e8a926a66e4164a360b6518
RajavJain <rajavjain2002@gmail.com>
2022-11-14 02:07:37 +0530

    Modified About Section

--- src/components/About.js
+++ src/components/About.js
@@ -20,7 +20,7 @@ const About = () => {
               </p>
             </div>
             <div className="col-md-5">
-              <img src="https://images.unsplash.com/photo-1621409791205-f3ce9280b5e9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fHRyYWRpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60" alt="" height="300px"
+              <img src="https://images.unsplash.com/photo-1517842645767-c639042777db?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bm90ZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60" alt="" height="300px"
                 width="500px"/>
                 <title>Placeholder</title>
                 <rect width="100%" height="100%" fill="#eee"></rect>
@@ -48,7 +48,7 @@ const About = () => {
                 </p>
               </div>
               <div className="col-md-5">
-                <img src="https://images.unsplash.com/photo-1638913662380-9799def8ffb1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxzZWFyY2h8MXx8dHJhZGluZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60" alt="" height="300px"
+                <img src="https://images.unsplash.com/photo-1503551723145-6c040742065b-v2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG5vdGVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" alt="" height="300px"
                   width="500px"/>
                   <title>Placeholder</title>
                   <rect width="100%" height="100%" fill="#eee"></rect>
@@ -77,7 +77,55 @@ const About = () => {
                   </p>
                 </div>
                 <div className="col-md-5">
+                  <img src="https://images.unsplash.com/photo-1605256585681-455837661b18?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fG5vdGVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" alt="" height="300px"
+                    width="500px"/>
+                    <title>Placeholder</title>
+                    <rect width="100%" height="100%" fill="#eee"></rect>
+                    <text x="50%" y="50%" fill="#aaa" dy=".3em"></text>
+                </div>
+              </div>
+            </div>
+
+            <hr className="featurette-divider"/>
+            <div className="container1 mr-10 d-flex justify-content-center align-items-center">
+              <div className="row featurette">
+                <div className="col-md-7 order-md-2">
+                  <h2 className="featurette-heading">
+                    Lorem, ipsum dolor.
+                    <span className="text-muted">It ll blow your mind.</span>
+                  </h2>
+                  <p className="lead">
+                    Some great placeholder content for the first featurette here.
+                    Imagine some exciting prose here.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci dolores magni ut. Saepe perferendis et placeat sapiente ut voluptatibus, quis magni nesciunt delectus, deserunt totam dolorum unde iste, aliquid corporis.
+                  </p>
+                </div>
+                <div className="col-md-5">
                   <img src="https://images.unsplash.com/photo-1563986768711-b3bde3dc821e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8dHJhZGluZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60" alt="" height="300px"
+                    width="500px"/>
+                    <title>Placeholder</title>
+                    <rect width="100%" height="100%" fill="#eee"></rect>
+                    <text x="50%" y="50%" fill="#aaa" dy=".3em"></text>
+                </div>
+              </div>
+            </div>
+
+            <hr className="featurette-divider"/>
+            <div className="container1 mr-10 d-flex justify-content-center align-items-center">
+              <div className="row featurette">
+                <div className="col-md-7">
+                  <h2 className="featurette-heading">
+                    Lorem, ipsum dolor.
+                    <span className="text-muted">It ll blow your mind.</span>
+                  </h2>
+                  <p className="lead">
+                    Some great placeholder content for the first featurette here.
+                    Imagine some exciting prose here.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci dolores magni ut. Saepe perferendis et placeat sapiente ut voluptatibus, quis magni nesciunt delectus, deserunt totam dolorum unde iste, aliquid corporis.
+                  </p>
+                </div>
+                <div className="col-md-5">
+                  <img src="https://media.istockphoto.com/id/1132083273/photo/thank-you-concepts-with-chat-speech-bubble-icons-on-blue-color-background.jpg?b=1&s=170667a&w=0&k=20&c=A-Ev5_zhO9P1Kzj9gkCq2u3E81cRdzXFpIubD-RUBII=" alt="" height="300px"
                     width="500px"/>
                     <title>Placeholder</title>
                     <rect width="100%" height="100%" fill="#eee"></rect>
