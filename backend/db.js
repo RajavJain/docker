dcdc7c0bafe58c1084b7c4e37c4ed27b2a670ada
RajavJain <rajavjain2002@gmail.com>
2023-01-20 08:10:06 +0530

    Edited Backend

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
 const mongoose=require('mongoose');
 const mongoURI="mongodb://localhost:27017/docket";
 
+
 const connectToMongo= () =>{   //made the function using mongoose to connect to mongodb
     mongoose.connect(mongoURI,() =>{
         console.log("Connected to mongoDB succesfully");
